0b2d5542851a23277af325ea8125d1cc0243c278
Hemavarun3 <21uec064@lnmiit.ac.in>
2025-04-15 19:10:12 +0530

    first commit

---
+++ src/main/java/net/project/journalApp/repository/JournalEntryRepository.java
@@ -0,0 +1,10 @@
+package net.project.journalApp.repository;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import net.project.journalApp.entity.JournalEntry;
+
+public interface JournalEntryRepository extends MongoRepository<JournalEntry, ObjectId> {
+
+}
